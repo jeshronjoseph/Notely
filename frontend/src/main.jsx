c38f40e7e8fcf8acfbcb6d8d83887759bccac045
jeshronjoseph <jeshron.joseph@gmail.com>
2026-05-22 23:47:02 +0530

    Route using render

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,7 +7,7 @@ import {Toaster} from "react-hot-toast";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/Notely">
+    <BrowserRouter>
       <App />
       <Toaster/>
     </BrowserRouter>
